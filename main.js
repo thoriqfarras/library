f33fcc0efa6d5515ea444f29d441b04aff75409c
thoriqHomePC <thoriqibrahimfarras@gmail.com>
2023-04-25 20:34:30 +0700

    add logic to add book to library

--- main.js
+++ main.js
@@ -1,9 +1,81 @@
-const name = "James";
+const editIcon = 'assets/pencil.svg';
+const deleteIcon = 'assets/delete.svg';
 
-const person = { first: name };
+function Book(title, author, page, status) {
+    this.title = title;
+    this.author = author;
+    this.page = page;
+    this.status = status;
+}
 
-console.log(person);
+Book.prototype.createCard = function createCard() {
+    const card = document.createElement('div');
+    const title = document.createElement('h2');
+    const author = document.createElement('p');
+    const pages = document.createElement('p');
+    const status = document.createElement('p');
+    const statusIndicator = document.createElement('span');
+    const buttons = document.createElement('div');
+    const editBtn = document.createElement('button');
+    const deleteBtn = document.createElement('button');
+    const editBtnIcon = document.createElement('img');
+    const deleteBtnIcon = document.createElement('img');
 
-const sayHelloLinting = (fName) => {
-  console.log(`Hello linting, ${fName}`);
+    // Set text content
+    title.textContent = this.title;
+    author.textContent = this.author;
+    pages.textContent = `Pages: ${this.page}`;
+    status.textContent = 'Status: ';
+    statusIndicator.textContent = this.status;
+
+    if (this.status === 'unread') {
+        statusIndicator.setAttribute('id', 'unread');
+    } else if (this.status === 'reading') {
+        statusIndicator.setAttribute('id', 'reading');
+    } else if (this.status === 'read') {
+        statusIndicator.setAttribute('id', 'read');
+    } else {
+        statusIndicator.style.color = '#09090b';
+    }
+
+    // set button
+    buttons.classList.add('buttons');
+
+    editBtn.setAttribute('type', 'button');
+    deleteBtn.setAttribute('type', 'button');
+    editBtn.setAttribute('id', 'edit');
+    deleteBtn.setAttribute('id', 'delete');
+
+    editBtnIcon.setAttribute('src', editIcon);
+    deleteBtnIcon.setAttribute('src', deleteIcon);
+
+    editBtn.appendChild(editBtnIcon);
+    deleteBtn.appendChild(deleteBtnIcon);
+
+    buttons.appendChild(editBtn);
+    buttons.appendChild(deleteBtn);
+
+    card.classList.add('card');
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(pages);
+    card.appendChild(status);
+    status.appendChild(statusIndicator);
+    card.appendChild(buttons);
+
+    return card;
 };
+
+const library = [];
+const collection = document.querySelector('.collection');
+
+function addBook(title, author, page, status) {
+    const book = new Book(title, author, page, status);
+    const card = book.createCard();
+    library.push(book);
+    collection.appendChild(card);
+}
+
+addBook('The Way of Kings', 'Brandon Sanderson', '764', 'read');
+addBook('Words of Radiance', 'Brandon Sanderson', '814', 'read');
+addBook('Oathbringer', 'Brandon Sanderson', '852', 'reading');
